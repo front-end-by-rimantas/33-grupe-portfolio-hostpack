2a64b10286b7f88f34fd8d191fd91ecc640da37c
Anton Sokolkin <97021906+ASreiros@users.noreply.github.com>
2022-02-18 13:17:35 +0200

    counter now responds to proximity

--- js/components/counter.js
+++ js/components/counter.js
@@ -1,19 +1,36 @@
 const counters = document.querySelectorAll('.counter');
 const speed = 15;
+let a = 2500;
+let width = screen.width;
 
-counters.forEach(counter => {
-    const updateNumbers = () => {
-        const target = +counter.getAttribute('data-target');
-        const count = +counter.innerText;
+if (width > 950){
+a = 2500;
+}
+if(width < 950){
+a = 3000;    
+}
+console.log(a);
+const myInterval = setInterval(runWhenClose,1000)
 
-        const increment = target / speed;
-       
-        if (count < target) {
-            counter.innerText = Math.trunc(count + increment);
-            setTimeout(updateNumbers, 40)
-        } else {
-            counter.innerText = target;
-        }
-    }
-    updateNumbers();
-});
+function runWhenClose() {
+    console.log("check");
+    if (window.pageYOffset > a) {
+        counters.forEach((counter) => {
+            const updateNumbers = () => {
+                const target = +counter.getAttribute('data-target');
+                const count = +counter.innerText;
+
+                const increment = target / speed;
+console.log("done");
+                if (count < target) {
+                    counter.innerText = Math.trunc(count + increment);
+                    setTimeout(updateNumbers, 40);
+                } else {
+                    counter.innerText = target;
+                }
+            };
+            updateNumbers();
+        });
+        clearInterval(myInterval);
+     }
+ }
