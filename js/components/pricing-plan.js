d474df953cc5eddbb39594c06d1156d8bb3f79cc
Bogdan Bylinski <bylinski.bogdan@gmail.com>
2022-02-20 15:43:27 +0200

    js addon

---
+++ js/components/pricing-plan.js
@@ -0,0 +1,48 @@
+// Checkbox: <input type="checkbox" id="myCheck1" onClick="javascript:uncheckShowYear1(this);">
+// Checkbox: <input type="checkbox" id="myCheck2" onClick="javascript:uncheckShowYear2(this);">
+// Checkbox: <input type="checkbox" id="myCheck3" onClick="javascript:uncheckShowYear3(this);">
+
+// <p>Click the "Try it" button to disable the checkbox.</p>
+
+// <button onclick="myFunction()">Try it</button>
+
+function xx1(obj) {
+    if (document.getElementById('sharing1').checked = true) {
+        document.getElementById('sharing2').checked = false;
+        document.getElementById('sharing3').checked = false;
+        document.getElementById('sharing4').checked = false;
+        document.getElementById('sharing5').checked = false;
+    }
+}
+function xx2(obj) {
+    if (document.getElementById('sharing2').checked = true) {
+        document.getElementById('sharing1').checked = false;
+        document.getElementById('sharing3').checked = false;
+        document.getElementById('sharing4').checked = false;
+        document.getElementById('sharing5').checked = false;
+    }
+}
+function xx3(obj) {
+    if (document.getElementById('sharing3').checked = true) {
+        document.getElementById('sharing1').checked = false;
+        document.getElementById('sharing2').checked = false;
+        document.getElementById('sharing4').checked = false;
+        document.getElementById('sharing5').checked = false;
+    }
+}
+function xx4(obj) {
+    if (document.getElementById('sharing4').checked = true) {
+        document.getElementById('sharing1').checked = false;
+        document.getElementById('sharing2').checked = false;
+        document.getElementById('sharing3').checked = false;
+        document.getElementById('sharing5').checked = false;
+    }
+}
+function xx5(obj) {
+    if (document.getElementById('sharing5').checked = true) {
+        document.getElementById('sharing1').checked = false;
+        document.getElementById('sharing2').checked = false;
+        document.getElementById('sharing3').checked = false;
+        document.getElementById('sharing4').checked = false;
+    }
+}
