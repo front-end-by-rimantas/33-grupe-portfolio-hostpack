64a7881d2cfebbda2f010313301383c6f453d5de
Anton Sokolkin <97021906+ASreiros@users.noreply.github.com>
2022-02-21 23:50:27 +0200

    faq caruseles konteineris

---
+++ js/components/popup.js
@@ -0,0 +1,10 @@
+function ShowPopUp(url)
+
+  {   
+
+  let disp_setting="menubar=no,";
+
+  disp_setting+="scrollbars=no,width=400, height=400, left=800, top=500, resizable=no";
+   window.open(url,"Kavos reklama",disp_setting);  
+
+  } 
