71a92f781cf98d99ae8621d2b3ddf849bc7bcdc6
Anton Sokolkin <97021906+ASreiros@users.noreply.github.com>
2022-02-23 01:19:55 +0200

    carousel done

--- js/components/slide-carousel.js
+++ js/components/slide-carousel.js
@@ -1,4 +1,4 @@
-let counter = 1;
+let counter = 0;
 const arrSlide = ['s1', 's2', 's3', 's4', 's5', 's6', 's7'];
 
 setInterval(slideClientsRight, 5000);
@@ -8,46 +8,72 @@ function slideClientsRight() {
     console.log('pong');
     for (let i = 0; i < arrSlide.length; i++) {
         const passive = document.getElementById(`${arrSlide[i]}`);
+        passive.classList.remove('activeslide-0');
         passive.classList.remove('activeslide-1');
         passive.classList.remove('activeslide-2');
         passive.classList.remove('activeslide-3');
+        passive.classList.remove('activeslide-4');
         console.log(arrSlide[i]);
     }
 
-    if (counter < 1) {
-        counter = 7;
+    if (counter < 0) {
+        counter = 6;
     }
-    if (counter > arrSlide.length) {
-        counter = 1;
+    if (counter > arrSlide.length-1) {
+        counter = 0;
     }
 
     let a = counter + 1;
-    if (a < 1) {
-        a = 7;
+    if (a < 0) {
+        a = 6;
     }
-    if (a > arrSlide.length) {
-        a = 1;
+    if (a > arrSlide.length-1) {
+        a = 0;
     }
 
     let b = a + 1;
-    if (b < 1) {
-        b = 7;
+    if (b < 0) {
+        b = 6;
     }
-    if (b > arrSlide.length) {
-        b = 1;
+    if (b > arrSlide.length-1) {
+        b = 0;
     }
-    const counterText = arrSlide[counter - 1];
-    const aText = arrSlide[a - 1];
-    const bText = arrSlide[b - 1];
 
-    console.log(counterText, aText, bText);
+    let c = b + 1;
+    if (c < 0) {
+        c = 6;
+    }
+    if (c > arrSlide.length-1) {
+        c = 0;
+    }
 
-    const active1 = document.getElementById(`${counterText}`);
+    let d = c + 1;
+    if (d < 0) {
+        d = 6;
+    }
+    if (d > arrSlide.length-1) {
+        d = 0;
+    }
+
+    const counterText = arrSlide[counter];
+    const aText = arrSlide[a];
+    const bText = arrSlide[b];
+    const cText = arrSlide[c];
+    const dText = arrSlide[d];
+
+
+    console.log(counterText, aText, bText, cText, dText);
+
+    const active0 = document.getElementById(`${counterText}`);
+    active0.classList.add('activeslide-0');
+    const active1 = document.getElementById(`${aText}`);
     active1.classList.add('activeslide-1');
-    const active2 = document.getElementById(`${aText}`);
+    const active2 = document.getElementById(`${bText}`);
     active2.classList.add('activeslide-2');
-    const active3 = document.getElementById(`${bText}`);
+    const active3 = document.getElementById(`${cText}`);
     active3.classList.add('activeslide-3');
-    console.log('done');
+    const active4 = document.getElementById(`${dText}`);
+    active4.classList.add('activeslide-4');
+
     counter++;
 }
