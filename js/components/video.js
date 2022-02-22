fbdab2907ea87b5f8418b26522ab236ae4d545aa
Anton Sokolkin <97021906+ASreiros@users.noreply.github.com>
2022-02-22 17:24:14 +0200

    link on front page

--- js/components/video.js
+++ js/components/video.js
@@ -1,12 +1,12 @@
 const btn = document.querySelector('.btn-video');
 const videoContainer = document.querySelector('.video-container');
 const frame = document.getElementById('youtube');
-// const close = document.querySelector('.xxxxx');
-const close = document.getElementById('closevideo');
+const close = document.querySelector('.xxxxx');
+// const close = document.getElementById('closevideo');
 btn.addEventListener('click', () => {
     videoContainer.classList.add('show');
     frame.innerHTML =
-        '<iframe  width="560" height="315" src="https://www.youtube.com/embed/f5pCYU3k360" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        '<iframe width="560" height="500" src="https://www.youtube.com/embed/ni5hRK1ehzk?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 });
 close.addEventListener('click', () => {
     videoContainer.classList.remove('show');
