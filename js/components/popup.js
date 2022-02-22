8cc5893ddc1945c59868f623531dcc2cae7d5ba8
Anton Sokolkin <97021906+ASreiros@users.noreply.github.com>
2022-02-22 15:10:02 +0200

    carousel slide done

--- js/components/popup.js
+++ js/components/popup.js
@@ -4,7 +4,7 @@ function ShowPopUp(url)
 
   let disp_setting="menubar=no,";
 
-  disp_setting+="scrollbars=no,width=400, height=400, left=800, top=500, resizable=no";
+  disp_setting+="scrollbars=no, titlebar = no, tollbar = no, width=600, height=500, left=200, top=200, resizable=no";
    window.open(url,"Kavos reklama",disp_setting);  
 
   } 
