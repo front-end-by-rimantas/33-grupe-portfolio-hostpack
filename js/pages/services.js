8962b1ac9e41d376148acce2f5d60d3a8977d632
SaKarolina <salnaite.k@gmail.com>
2022-02-27 15:41:49 +0200

    hidden menu

--- js/pages/services.js
+++ js/pages/services.js
@@ -3,12 +3,38 @@
 // CODE EXECUTION BELOW THIS COMMENT LINE
 
 /* header startas */
-const hamburgerS = document.querySelector(`.hamburger-srv`);
-const menuS = document.querySelector(`#menuSrv`);
+const hamburgerS = document.querySelector(`.burger-bar`);
+const menuS = document.querySelector(`.dropdown2-srv`);
+const link1 = document.getElementById(`menu1`);
+const link2 = document.getElementById(`menu2`);
+const link3 = document.getElementById(`menu3`);
+const link4 = document.getElementById(`menu4`);
+const link5 = document.getElementById(`menu5`);
+const menu1 = document.querySelector(`.homeMenu`);
+const menu2 = document.querySelector(`.pagesMenu`);
+const menu3 = document.querySelector(`.hostingMenu`);
+const menu4 = document.querySelector(`.servMenu`);
+const menu5 = document.querySelector(`.priceMenu`);
 
 hamburgerS.addEventListener('click', () => {
-    menuS.classList.toggle(`list2-srv`);
+    menuS.classList.toggle(`visible-menuSrv`);
 });
+link1.addEventListener('click', () => {
+    menu1.classList.toggle(`visible-1`);
+});
+link2.addEventListener('click', () => {
+    menu2.classList.toggle(`visible-2`);
+});
+link3.addEventListener('click', () => {
+    menu3.classList.toggle(`visible-3`);
+});
+link4.addEventListener('click', () => {
+    menu4.classList.toggle(`visible-4`);
+});
+link5.addEventListener('click', () => {
+    menu5.classList.toggle(`visible-5`);
+});
+
 /*-----------------*/
 window.onscroll = function () {
     topukas();
